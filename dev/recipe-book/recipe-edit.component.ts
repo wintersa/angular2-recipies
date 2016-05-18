885d14fd9ac8027fe8f824f301b40c6914446afe
Anthony Winters <a.r.winters@nforce-it.nl>
2016-05-18 02:34:24 +0200

    Error Safe Navigation

--- dev/recipe-book/recipe-edit.component.ts
+++ dev/recipe-book/recipe-edit.component.ts
@@ -1,5 +1,6 @@
 import {Component} from  'angular2/core';
 import {OnInit} from  'angular2/core';
+import {Router} from 'angular2/router';
 import {RouteParams} from 'angular2/router';
 import {Control} from  'angular2/common';
 import {ControlGroup} from  'angular2/common';
@@ -8,19 +9,23 @@ import {Validators} from  'angular2/common';
 import {FormBuilder} from  'angular2/common';
 import {Recipe} from  "../shared/recipe";
 import {RecipeService} from "./recipe.service";
+import {CanDeactivate} from 'angular2/router';
+import {ComponentInstruction} from 'angular2/router';
+
 
 @Component({
     templateUrl: 'templates/recipes/recipe-edit.tpl.html'
 })
 
-export class RecipeEditComponent implements OnInit {
+export class RecipeEditComponent implements OnInit, CanDeactivate {
     myForm: ControlGroup;
     recipe: Recipe;
     private _editMode = 'create';
     private _recipeIndex: number;
+    private _submitted = false;
 
     constructor(private _routeParams: RouteParams, private _recipeService: RecipeService,
-                private _formBuilder: FormBuilder) {}
+                private _formBuilder: FormBuilder, private _router: Router) {}
 
     onAddItem(itemName: string, itemAmount: string) {
         (<ControlArray>this.myForm.controls['ingredients']).push(
@@ -48,6 +53,16 @@ export class RecipeEditComponent implements OnInit {
         } else {
             this._recipeService.insertRecipe(this.recipe);
         }
+        this._submitted = true;
+        this.navigateBack();
+    }
+
+    onCancel() {
+        this.navigateBack();
+    }
+
+    private navigateBack() {
+        this._router.navigate(['RecipeDetail', {recipeIndex: this._recipeIndex}]);
     }
 
     ngOnInit():any {
@@ -84,6 +99,14 @@ export class RecipeEditComponent implements OnInit {
             content: [fbRecipeContent],
             ingredients: this._formBuilder.array(fbIngredients.controls)
         });
+    }
+
+    routerCanDeactivate(nextInstruction: ComponentInstruction, prevInstruction: ComponentInstruction)
+    {
+      if (this._submitted || this.myForm.pristine) {
+          return true;
+      }
+      return confirm("You loose everyting, everything!!!");
     }
 }
 
